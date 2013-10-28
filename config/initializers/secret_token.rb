e54574fa67d2149dc4c927b840828e15bf18b4b0
Patrick Baselier <patrick@kabisa.nl>
2013-10-28 21:25:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoundationPrototypingWRails::Application.config.secret_key_base = '44d424c7818e73e40fdbec4f5d5389899e5d867364ed1b62f332be7c095a71b68f10bf3260e89d22ba030faf95eaba1a90db8d9497d77569d26dc463e5502bc5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoundationPrototypingWRails::Application.config.session_store :cookie_store, key: '_foundation-prototyping-w-rails_session'
